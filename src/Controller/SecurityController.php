c42147e4531eeb323fcde5d31de15cb54a8d1555
Marlene Tissier <marlene.tissier@lapiscine.pro>
2020-07-28 16:35:37 +0200

    potentielle merde

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,9 +14,13 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        $user = $this->getUser();
+
+        if ($user->hasRole('ROLE_ADMIN')) {
+             return $this->redirectToRoute('HomeAdmin');
+         } elseif ($user->hasRole('ROLE_ADMIN')){
+             return $this->redirectToRoute('Home');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -71,7 +71,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            throw new CustomUserMessageAuthenticationException('Email incorrect');
         }
 
         return $user;
@@ -97,7 +97,11 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TO DO: provide a valid redirect inside'.__FILE__);
+
+        // redirect to some "app_homepage" route - of wherever you want
+        return new RedirectResponse($this->urlGenerator->generate('Home'));
+
     }
 
     protected function getLoginUrl()
